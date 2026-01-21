36909406591c5ddc892ac3846631104be078d395
Om Umesh Ratnaparkhe <omsaysless@Oms-MacBook-Air.local>
2026-01-21 22:30:17 +0530

    Update frontend deployed url

--- Backend/server.js
+++ Backend/server.js
@@ -6,9 +6,10 @@ import  recordingRoutes  from "./routes/recordings.js";
 
 const app = express();
 const frontend_url_dev = process.env.FRONTEND_URL_DEV || "http://localhost:5173"
-const frontend_url_prod = process.env.FRONTEND_URL_PROD;
+const frontend_url_prod = process.env.FRONTEND_URL_PROD || "https://we-meet-eight.vercel.app";
+
 app.use(cors({
-  origin:frontend_url_dev,
+  origin:frontend_url_prod,
   credentials:true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization","x-clerk-id"],
